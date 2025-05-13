5c30149384efbf1006378ab114ceab3d049fbd62
“Anjali <anjali.cspatidar@gmail.com>
2025-05-13 14:17:08 -0400

    structure

---
+++ src/app/portfolio/portfolio-view/portfolio-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-view.component.html',
+  styleUrl: './portfolio-view.component.css'
+})
+export class PortfolioViewComponent {
+
+}
